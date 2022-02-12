8130ed8a68a03f3425c18af57e03c9a9b0762a11
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-02-12 06:19:49 +0100

    Modificacion del GetSuggestion (incompleto)
    Tengo la implementacion del GetSuggestion pero tengo que ver todavia que opcion y que palabra escoger en cada caso(sinonimos o LevenshteinDistance)
    y ver como mostrar un suggestion valido

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -8,7 +8,7 @@ public static class Moogle
     {
 
         // ! Calcular el suggestion por las palabras que no aparecen en el documento
-        (string suggestion, query ) = GetSuggestion(query);
+        string suggestion = GetSuggestion(query);
 
         //! Frecuencia de las palabras de la query
         Dictionary<string, int> FreqWordsQuery = GetFreqWordsInQuery( query );
@@ -40,7 +40,7 @@ public static class Moogle
         NormalizeData(MemoryChange);
 
         // //! Si no ubieron palabras mal escritas entonces no hay que mostrar sugerencia
-        // if(suggestion == query) suggestion = ""; 
+        if(suggestion == query) suggestion = ""; 
 
         return new SearchResult(items, suggestion);
     }
@@ -100,9 +100,8 @@ public static class Moogle
         
         return sim;
     }
-    private static (string, string) GetSuggestion(string query) {
+    private static string GetSuggestion(string query) {
         string suggestion = "";
-        string newQuery = "";
 
         string[] words = AuxiliarMethods.GetWordsOfSentence(query);
         List< Tuple<string, string> > operators = WorkingOperators.GetOperators(query);
@@ -119,35 +118,77 @@ public static class Moogle
             string w = AuxiliarMethods.NormalizeWord(AuxiliarMethods.GetWordStartIn(query, i));
             string lemman = Lemmatization.Stemmer(w);
 
-            // Si la palabra esta en el documento entonces no hay que modificarla
+            //* Si la palabra esta en el documento entonces no hay que modificarla
             if(Data.IdxWords.ContainsKey(lemman)) {
                 suggestion += w;
                 i += w.Length - 1;
                 continue;
             }
 
-            // En caso de que no este
-            // !Antes de usar Levenshtein vemos si tiene sinonimos en la BD -----------------------------------------------------------------------------
-            
+            //* En caso de que no este busco todos los sinonimos de esa palabra en la base de datos
+            string[] Syn = Data.Synonyms.GetSynonymsOf(w);
 
+            float bestScore = (float)int.MinValue;
+            float secondBestScore = (float)int.MinValue;
+            string bestWord = "";
+            string secondBestWord = "";
 
+            // Quedarme con el que mayor score tiene entre todos los documentos
+            foreach(string sin in Syn) {
+                string lem = Lemmatization.Stemmer(sin);
 
+                // Si no esta en ningun documento lo omito
+                if( !Data.IdxWords.ContainsKey(lem) ) continue;
 
-            string newW = "";
-            int steps = 100000;
-            foreach(KeyValuePair<string, int> wd in Data.IdxWords) {
-                int cost = AuxiliarMethods.LevenshteinDistance(w, wd.Key);
-                if(cost <= steps) {
-                    steps = cost;
-                    newW = wd.Key;
+                // Calculo el score promedio de esa palabra entre todos los documentos
+                int idx = Data.IdxWords[ lem ];
+                float ScorePromedio = 0.00f;
+
+                for(int doc = 0; doc < Data.TotalFiles; doc ++) 
+                    ScorePromedio += Data.wDocs[doc, idx];
+                ScorePromedio /= Data.TotalWords;
+
+                if(bestScore < ScorePromedio) {
+                    bestScore = ScorePromedio;
+                    bestWord = sin;
                 }
             }
-            suggestion += newW;
-            i += w.Length - 1;
+
+            // Si encontre un sinonimo para sustituir la palabra que no esta
+            if(bestWord != "") {
+                System.Console.WriteLine(bestWord);
+                suggestion += bestWord;
+                i += w.Length - 1;
+                continue;
+            }
+
+
+            // * Si tampoco esta en la base de datos entonces veo que esta mas escrita
+            foreach(KeyValuePair<string, int> wd in Data.IdxWords) {
+                int cost = AuxiliarMethods.LevenshteinDistance(w, wd.Key);
+
+                float umbralChange = (4.00f / 10.00f) * (float)wd.Key.Length;    // 40% de la palabra es el tope maximo a cambiar
+
+                if(cost < secondBestScore) {
+                    secondBestScore = cost;
+                    secondBestWord = wd.Key;
+                }
+
+                if( (float)cost >= umbralChange ) continue;
+
+                if(cost < bestScore) {
+                    bestScore = cost;
+                    bestWord = wd.Key;
+                }
+            }
+
+            System.Console.WriteLine(bestWord);
+            suggestion += (bestWord != "") ? bestWord : secondBestWord;
+             i += w.Length - 1;
        }
 
 
-        return (suggestion, newQuery);
+        return suggestion;
     }
     private static SearchItem[] BuildResult( Tuple<float, int>[] sim, Dictionary<string, int> FreqWordsQuery, float[,] wDocs) {
         List<SearchItem> items = new List<SearchItem>();
