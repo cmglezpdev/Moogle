49de05301e5d6940eca79ad61b3b64979b300ca1
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-02-12 06:16:27 +0100

    Adding implementacion para que devuelva los sinonimos de una palabra

--- MoogleEngine/WorkingSynonyms.cs
+++ MoogleEngine/WorkingSynonyms.cs
@@ -26,6 +26,35 @@ public class WorkingSynonyms {
         get{return this.Synonyms.Count;}
     }
 
+    public string[] GetSynonymsOf( string word ) {
+        
+        List<string> sinonimos = new List<string>();
+
+
+        int n = this.LengthDB;
+
+        // Ir por cada array de conjuntos de sinonimos
+        for(int i = 0; i < n; i ++) {
+
+            bool found = false;
+
+            // Ir por cada palabra de ese conjunto y ver si es la palabra que me interesa
+            foreach(string w in this.Synonyms[i]) {
+                if( Lemmatization.Stemmer( w ) != word ) continue;
+                found = true;
+                break;
+            }
+
+            if(!found) continue;
+            foreach(string w in this.Synonyms[i]) {
+                if( Lemmatization.Stemmer( w ) == word ) continue;
+                sinonimos.Add(w);
+            }
+        }
+
+        // Se devuelve un arreglo vacio xq no se encontro nada
+        return sinonimos.ToArray();
+    }
 
 
 
